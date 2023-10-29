e7f50aeef50e91511ef8f9ce336fbca251e45e21
Memory-Wabwile <memorywabwile@gmail.com>
2023-10-29 20:17:16 +0300

    save changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :properties
-  devise_for :accounts
+  devise_for :account
   root to: 'properties#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
